b366a1a3366898dfa8c6c6a16ed928eddf2abb38
Adrian 'nex' Schollmeyer <adrian.schollmeyer@fem.tu-ilmenau.de>
2022-07-08 16:53:27 +0200

    vprint: Always take const references
    Ensures any type of reference (especiall rvalue references) can be
    passed.

--- src/vprint.hpp
+++ src/vprint.hpp
@@ -7,10 +7,10 @@ extern bool vprint_verbose;
 namespace
 {
 template <typename... T>
-void vprint(T&... args)
+void vprint(const T&... args)
 {
 	if (vprint_verbose) {
-		(std::cout << ... << std::forward<T>(args));
+		(std::cout << ... << std::forward<const T>(args));
 	}
 }
 } // namespace
